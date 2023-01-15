b5c7c75a2813b314f4980e19e3f9b91b056ecbaa
lofiamity <108965582+lofiamity@users.noreply.github.com>
2023-01-16 05:32:54 +0700

    Update ᒲᔑ╎リ.py

--- ᒲᔑ╎リ.py
+++ ᒲᔑ╎リ.py
@@ -1,14 +1,22 @@
-import telebot
+5879583968:AAEcefwcNIz_ALskE9OOZs744URsZa2TJ_o
+    
 import pyshorteners
+import telebot
 
-bot = telebot.TeleBot("5879583968:AAEcefwcNIz_ALskE9OOZs744URsZa2TJ_o")
-
+bot = telebot.TeleBot("YOUR_BOT_TOKEN")
 s = pyshorteners.Shortener()
 
 @bot.message_handler(commands=['short'])
 def shorten_url(message):
-    url = message.text.split()[1]
-    shortened_url = s.tinyurl.short(url)
-    bot.reply_to(message, shortened_url)
+    if len(message.text.split()) == 1:
+        bot.reply_to(message, "Invalid command. Please include a URL to shorten after the '/short' command.")
+    else:
+        url = message.text.split()[1]
+        shortened_url = s.tinyurl.short(url)
+        bot.reply_to(message, shortened_url)
+
+@bot.message_handler(func=lambda message: not message.text.startswith('/'))
+def send_help(message):
+    bot.reply_to(message, "Please use /short command followed by URL you want to shorten")
 
 bot.polling()
